19426e1d8152e22528329e3a9648f3625038dd99
pattaranan srisara <pattaranan.s@kpi.co.th>
2021-07-01 11:37:49 +0700

    Add AJAX request to check status for redirect

--- views.py
+++ views.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+app.run(debug=True)
 
+_check = 0 
 @app.route('/')
 def qrcode():
     return render_template('qrcode.html')
+
+@app.route('/check-status')
+def check():
+    global _check   
+    _check += 1
+    if (_check == 10):
+        return jsonify({'status': 'success'})
+    return jsonify({'status': 'fail'})
 
 @app.route('/next')
 def next():
